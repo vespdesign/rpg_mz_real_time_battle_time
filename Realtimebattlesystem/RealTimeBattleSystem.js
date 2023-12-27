3eab6349100f7748e843915ef9d20a3e1d75e01b
vespdesign <victorespinosa@vespdesign.es>
2023-12-28 00:45:06 +0100

    Create RealTimeBattleSystem.js

---
+++ Realtimebattlesystem/RealTimeBattleSystem.js
@@ -0,0 +1,71 @@
+/*:
+ * @target MZ
+ * @plugindesc Sistema de Batalla en Tiempo Real Básico
+ * @author GeekGames
+ *
+ * @help
+ * Este plugin implementa un sistema de batalla en tiempo real básico.
+ * VERSION DE PRUEBA ALPHA 0.11
+ */
+
+(function() {
+    // Constantes para la configuración del sistema
+    const DISTANCIA_DE_ATAQUE = 1;   // Distancia para iniciar el ataque
+    const DISTANCIA_DE_PERSECUCION = 50;  // Distancia para que el enemigo persiga al jugador
+    const ENEMY_DAMAGE = 10; // Cantidad de daño que el enemigo hace al jugador
+
+    // Sobrescribir la actualización del evento del enemigo
+    const originalGameEventUpdate = Game_Event.prototype.update;
+    Game_Event.prototype.update = function() {
+        // Llamada original a la actualización del evento
+        originalGameEventUpdate.call(this);
+
+        // Verificar si el evento tiene el comentario "battlesystem"
+        if (this.tieneComentarioBattleSystem()) {
+            // Lógica de detección y ataque
+            this.verificarDistanciaYActuar();
+        }
+    };
+
+    // Verificar si el evento contiene el comentario "battlesystem"
+    Game_Event.prototype.tieneComentarioBattleSystem = function() {
+        const list = this.page().list;
+        for (const command of list) {
+            if (command.code === 108 || command.code === 408) {
+                if (command.parameters[0].includes("battlesystem")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+
+    // Función para verificar la distancia y realizar una acción (atacar o perseguir)
+    Game_Event.prototype.verificarDistanciaYActuar = function() {
+        const distanciaAlJugador = $gamePlayer.distanceTo(this);
+
+        // Si el jugador está dentro del rango de ataque
+        if (distanciaAlJugador <= DISTANCIA_DE_ATAQUE) {
+            this.atacarJugador();
+        }
+        // Si el jugador está dentro del rango de persecución pero fuera del rango de ataque
+        else if (distanciaAlJugador <= DISTANCIA_DE_PERSECUCION) {
+            this.perseguirJugador();
+        }
+    };
+
+    // Función para atacar al jugador
+    Game_Event.prototype.atacarJugador = function() {
+        // Lógica de ataque: reduce la salud del jugador
+        $gameActors.actor(1).setHp($gameActors.actor(1).hp - ENEMY_DAMAGE);
+        console.log("El enemigo ataca al jugador!");
+    };
+
+    // Función para que el enemigo persiga al jugador
+    Game_Event.prototype.perseguirJugador = function() {
+        // Implementación básica de la lógica para mover el enemigo hacia el jugador
+        this.moveTowardPlayer();
+        console.log("El enemigo persigue al jugador");
+    };
+
+})();
