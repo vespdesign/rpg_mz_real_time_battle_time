d283da2ea3173989e6c0b727e9704ec9822d61b2
vespdesign <victorespinosa@vespdesign.es>
2023-12-28 16:55:51 +0100

    0.212

--- Realtimebattlesystem/RealTimeBattleSystem.js
+++ Realtimebattlesystem/RealTimeBattleSystem.js
@@ -5,7 +5,7 @@
  *
  * @help
  * Este plugin implementa un sistema de batalla en tiempo real básico.
- * VERSION DE PRUEBA ALPHA 0.21
+ * VERSION DE PRUEBA ALPHA 0.212
  */
 
 (function() {
@@ -72,75 +72,68 @@
 
     // Función para moverse aleatoriamente o volver a la zona original
     Game_Event.prototype.moverAleatoriamenteOVolver = function() {
-        // Implementar la lógica de movimiento aquí
+        if (this.estáFueraDeSuZona()) {
+            this.volverAZonaOriginal();
+        } else {
+            this.moveRandomlyInArea();
+        }
     };
 
-    // Función para moverse aleatoriamente o volver a la zona original
-	Game_Event.prototype.moverAleatoriamenteOVolver = function() {
-    if (this.estáFueraDeSuZona()) {
-        this.volverAZonaOriginal();
-    } else {
-        this.moveRandomlyInArea();
-    }
-	};
-	
-	// Sobrescribir la inicialización de miembros del evento
-	const originalGameEventInitMembers = Game_Event.prototype.initMembers;
-	Game_Event.prototype.initMembers = function() {
-    originalGameEventInitMembers.call(this);
-    this._health = 100; // Valor predeterminado de la salud del enemigo
-    this._startX = this.x; // Almacenar la posición inicial X
-    this._startY = this.y; // Almacenar la posición inicial Y
-};
+    // Sobrescribir la inicialización de miembros del evento
+    const originalGameEventInitMembers = Game_Event.prototype.initMembers;
+    Game_Event.prototype.initMembers = function() {
+        originalGameEventInitMembers.call(this);
+        this._health = 100; // Valor predeterminado de la salud del enemigo
+        this._startX = this.x; // Almacenar la posición inicial X
+        this._startY = this.y; // Almacenar la posición inicial Y
+    };
 
-};
-	
-	// Verificar si el enemigo está fuera de su área original
-	Game_Event.prototype.estáFueraDeSuZona = function() {
-		const dx = Math.abs(this.x - this.startX());
-		const dy = Math.abs(this.y - this.startY());
-		return dx > 4 || dy > 4;
-	};
+    // Verificar si el enemigo está fuera de su área original
+    Game_Event.prototype.estáFueraDeSuZona = function() {
+        const dx = Math.abs(this.x - this._startX);
+        const dy = Math.abs(this.y - this._startY);
+        return dx > 4 || dy > 4;
+    };
 
-	// Moverse aleatoriamente dentro del rango definido
-	Game_Event.prototype.moveRandomlyInArea = function() {
-    const dx = Math.floor(Math.random() * 9) - 4; // Rango de -4 a 4
-    const dy = Math.floor(Math.random() * 9) - 4; // Rango de -4 a 4
-    const targetX = this._startX + dx;
-    const targetY = this._startY + dy;
+    // Moverse aleatoriamente dentro del rango definido
+    Game_Event.prototype.moveRandomlyInArea = function() {
+        const dx = Math.floor(Math.random() * 9) - 4; // Rango de -4 a 4
+        const dy = Math.floor(Math.random() * 9) - 4; // Rango de -4 a 4
+        const targetX = this._startX + dx;
+        const targetY = this._startY + dy;
 
-    if ($gameMap.isValid(targetX, targetY)) {
-        this.moveStraight(this.findDirectionTo(targetX, targetY));
-    }
-	};
-
-	// Volver a la zona original
-	Game_Event.prototype.volverAZonaOriginal = function() {
-    if (this.x !== this._startX || this.y !== this._startY) {
-        this.moveStraight(this.findDirectionTo(this._startX, this._startY));
-    }
-	};
-
-	// Atacar al objetivo más cercano (jugador u otro enemigo)
-	Game_Event.prototype.atacarObjetivoMasCercano = function() {
-		let objetivos = [$gamePlayer].concat($gameMap.events().filter(event => event.tieneComentarioBattleSystem() && event !== this));
-		let objetivoMasCercano = objetivos.reduce((prev, current) => {
-        return this.distanceTo(current) < this.distanceTo(prev) ? current : prev;
-    });
-
-    // Determinar si el objetivo más cercano está dentro del rango de ataque
-    if (this.distanceTo(objetivoMasCercano) <= DISTANCIA_DE_ATAQUE) {
-        // Implementar lógica de ataque
-        if (objetivoMasCercano === $gamePlayer) {
-            // Atacar al jugador
-            $gameActors.actor(1).setHp($gameActors.actor(1).hp - ENEMY_DAMAGE);
-            console.log("El enemigo ataca al jugador!");
-        } else {
-            // Atacar a otro enemigo
-            // Aquí necesitarás una forma de reducir la salud del enemigo atacado, que dependerá de cómo estés manejando su salud.
-            console.log("El enemigo ataca a otro enemigo!");
+        if ($gameMap.isValid(targetX, targetY)) {
+            this.moveStraight(this.findDirectionTo(targetX, targetY));
         }
-    }
-	};
+    };
+
+    // Volver a la zona original
+    Game_Event.prototype.volverAZonaOriginal = function() {
+        if (this.x !== this._startX || this.y !== this._startY) {
+            this.moveStraight(this.findDirectionTo(this._startX, this._startY));
+        }
+    };
+
+    // Atacar al objetivo más cercano (jugador u otro enemigo)
+    Game_Event.prototype.atacarObjetivoMasCercano = function() {
+        let objetivos = [$gamePlayer].concat($gameMap.events().filter(event => event.tieneComentarioBattleSystem() && event !== this));
+        let objetivoMasCercano = objetivos.reduce((prev, current) => {
+            return this.distanceTo(current) < this.distanceTo(prev) ? current : prev;
+        });
+
+        // Determinar si el objetivo más cercano está dentro del rango de ataque
+        if (this.distanceTo(objetivoMasCercano) <= DISTANCIA_DE_ATAQUE) {
+            // Implementar lógica de ataque
+            if (objetivoMasCercano === $gamePlayer) {
+                // Atacar al jugador
+                $gameActors.actor(1).setHp($gameActors.actor(1).hp - ENEMY_DAMAGE);
+                console.log("El enemigo ataca al jugador!");
+            } else {
+                // Atacar a otro enemigo
+                // Aquí necesitarás una forma de reducir la salud del enemigo atacado, que dependerá de cómo estés manejando su salud.
+                console.log("El enemigo ataca a otro enemigo!");
+            }
+        }
+    };
 
 })();
